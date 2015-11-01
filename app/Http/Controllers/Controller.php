30a4d9c2291e33db3eaab2037c1977eb546441cb
Frank Kung <kfanjian@gmail.com>
2015-11-01 21:27:00 +0800

    init ssdb connect instance.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use SSDB;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public $ssdb = null;
+    public $db = null;
+
+    public function __construct()
+    {
+        // get ssdb database config.
+        $this->db = config('database.ssdb.default');
+        // init ssdb.
+        $this->ssdb = new SSDB\Client($this->db['host'], $this->db['port']);
+    }
 }
