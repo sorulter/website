9d8005a5903279ef2dc7cfd77c8252dab576c7fc
Frank Kung <kfanjian@gmail.com>
2015-11-02 23:21:34 +0800

    add action to process login post.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -18,13 +18,13 @@ class LoginController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Process login post action.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function postIndex(Request $request)
     {
-        //
+        $input = $request->input();
     }
 
     /**
