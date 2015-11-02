964b0c9d16e4488b4526cc71956c403e2ce53035
Frank Kung <kfanjian@gmail.com>
2015-11-02 23:52:22 +0800

    modify login form to disable submit.

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -35,9 +35,9 @@
     <div class="login-box-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="{{ route('user/login') }}" method="post">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      <form>
         <div class="form-group has-feedback">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
           <input type="text" class="form-control" placeholder="Username" name="username" id="username">
           <span class="glyphicon glyphicon-user form-control-feedback"></span>
         </div>
@@ -45,21 +45,21 @@
           <input type="password" class="form-control" placeholder="Password" name="password" id="password">
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
-        <div class="row">
-          <div class="col-xs-8">
-            <div class="checkbox icheck">
-              <label>
-                <input type="checkbox" name="remember"> Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-xs-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat" id="submit" disabled>Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
       </form>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox" name="remember"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat" id="submit" disabled>Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
 
       <a href="#">I forgot my password</a>&emsp;|&emsp;<a href="#" class="text-center">Register a new membership</a>
 
