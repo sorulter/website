ee93ee018f608e0a3629d3b2591b69f30f84abd3
Frank Kung <kfanjian@gmail.com>
2015-11-03 21:51:49 +0800

    add user home page controller.

---
+++ app/Http/Controllers/User/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Display user home.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getIndex()
+    {
+    }
+
+}
