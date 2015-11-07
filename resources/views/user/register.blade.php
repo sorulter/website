2294bd7b4c63a8b15651fb2649dc250d63754a6d
Frank Kung <kfanjian@gmail.com>
2015-11-07 13:43:19 +0800

    add shake method to jQuery.

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -113,6 +113,18 @@
     $(element).parent().removeClass('has-error').addClass('has-success');
   }
 
+  jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+      $(this).css("position","relative");
+      for (var x=1; x<=intShakes; x++) {
+        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+        .animate({left:intDistance}, ((intDuration/intShakes)/2))
+        .animate({left:0}, (((intDuration/intShakes)/4)));
+      }
+    });
+    return this;
+  };
+
   </script>
   </body>
 </html>
