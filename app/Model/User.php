cae83cd9c10449918a27842f7e1e180f3bb6d7de
Frank Kung <kfanjian@gmail.com>
2015-11-21 02:10:35 +0800

    add register method to user model.

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,6 +3,7 @@
 class User extends Base
 {
     private $MailNS = 'user.mail.';
+    private $PasswordNS = 'user.password.';
 
     /**
      * Check the mail is exist.If exist return true
@@ -16,5 +17,31 @@ class User extends Base
     {
         $mail = $this->_ssdb->get($this->_ns . $this->MailNS . $mail);
         return $mail->data != null;
+    }
+
+    /**
+     * Register new user.
+     *
+     * @since 2015-11-20 02:09:32
+     * @param String $mail
+     * @param String $password
+     *
+     * @return Boolean
+     */
+    public function register($mail, $password)
+    {
+        $data2store = [
+            $this->_ns . $this->MailNS . $mail, 0,
+            $this->_ns . $this->PasswordNS . $mail, $password,
+        ];
+        $rs = $this->_ssdb->multi_set($data2store);
+
+        if ($rs->data == sizeof($data2store) / 2 && $rs->data == 'ok') {
+            return true;
+        } else {
+            return false;
+        }
+
+        dd($data2store, $rs);
     }
 }
