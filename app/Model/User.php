dfbd01dbb00e91598b96f85d0dc4ca00496229a1
Frank Kung <kfanjian@gmail.com>
2015-11-21 02:09:06 +0800

    modify info.

--- app/Model/User.php
+++ app/Model/User.php
@@ -4,13 +4,14 @@ class User extends Base
 {
     private $MailNS = 'user.mail.';
 
-/**
- *
- * Check the mail is exist.If exist return true
- *
- * @param String $mail
- * @return Boolean
- */
+    /**
+     * Check the mail is exist.If exist return true
+     *
+     * @since 2015-11-20 02:07:50
+     * @param String $mail
+     *
+     * @return Boolean
+     */
     public function has($mail)
     {
         $mail = $this->_ssdb->get($this->_ns . $this->MailNS . $mail);
