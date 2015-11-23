af3f314f83a3b614eea7c989eecd18b2c1be2d33
Frank Kung <kfanjian@gmail.com>
2015-11-24 01:01:08 +0800

    add welcome mail view.

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head>
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Activate your iProxier account to start surfing</title>
+<style type="text/css">
+img {
+max-width: 100%;
+}
+body {
+font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; height: 100% !important; line-height: 1.6; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; width: 100% !important;
+}
+body {
+background-color: #f6f6f6;
+}
+@media only screen and (max-width: 620px) {
+  table[class=body] h1 {
+    font-weight: 600 !important;
+  }
+  table[class=body] h2 {
+    font-weight: 600 !important;
+  }
+  table[class=body] h3 {
+    font-weight: 600 !important;
+  }
+  table[class=body] h4 {
+    font-weight: 600 !important;
+  }
+  table[class=body] h1 {
+    font-size: 22px !important;
+  }
+  table[class=body] h2 {
+    font-size: 18px !important;
+  }
+  table[class=body] h3 {
+    font-size: 16px !important;
+  }
+  table[class=body] .wrapper {
+    padding: 10px !important;
+  }
+  table[class=body] .content {
+    padding: 0 !important;
+  }
+  table[class=body] .container {
+    padding: 0 !important; width: 100% !important;
+  }
+  table[class=body] .btn {
+    width: 100% !important;
+  }
+}
+</style>
+</head>
+<body><table class="body" style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-spacing: 0; width: 100%; background: #f6f6f6;" width="100%"><tbody><tr>
+<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top"></td>
+		<td class="container" style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: block; max-width: 580px; width: 580px; margin: 0 auto; padding: 10px;" valign="top">
+			<div class="content" style="box-sizing: border-box; display: block; max-width: 580px; margin: 0 auto; padding: 10px;">
+<table class="main" style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 3px; border-spacing: 0; width: 100%; background: #fff; border: 1px solid #e9e9e9;" width="100%"><tbody><tr>
+<td class="wrapper" style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 30px;" valign="top">
+			<table style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tbody><tr>
+<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top">
+						<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">Thanks for choosing iProxier. Please activate your account by clicking the button below.</p>
+						<table class="btn btn-primary" style="box-sizing: border-box; -premailer-width: auto; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; margin-bottom: 15px; width: auto;" width="auto"><tbody><tr>
+<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 5px; text-align: center; background: #348eda;" align="center" valign="top">
+              		<a href="http://iProxier.com/activate/{{ $code }}" style="box-sizing: border-box; border-radius: 5px; color: #fff; text-decoration: none; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; text-transform: capitalize; background: #348eda; margin: 0; padding: 12px 25px; border: 1px solid #348eda;">Activate iProxier account</a>
+                </td>
+              </tr></tbody></table>
+              <p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">This activate link will disable after <strong>15 minutes</strong>.</p>
+<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">We may need to communicate important service level issues with you from time to time, so it's <strong>important we have an up-to-date email address</strong> for you on file.</p>
+						<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">Thanks and happy surfing, The iProxier.</p>
+					</td>
+				</tr></tbody></table>
+</td>
+	</tr></tbody></table>
+</div>
+		</td>
+		<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top"></td>
+	</tr></tbody></table></body>
+</html>
