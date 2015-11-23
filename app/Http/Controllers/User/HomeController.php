c689fb210e50c0801f9682be7c8466f63517af50
Frank Kung <kfanjian@gmail.com>
2015-11-24 01:01:43 +0800

    add controller method to resent activate mail.

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Model\User;
 use Session;
 
 class HomeController extends Controller
@@ -34,6 +35,26 @@ class HomeController extends Controller
             ->withType('warning')->withTitle('Please Activate Your Account')
             ->withContent('<p>Before you can login, you must active your account with the code sent to your email address.</p>
             <p>If you did not receive this email, please check your junk/spam folder.Click <a href="/user/activate/resent">here</a> to resend the activation email.</p>');
+    }
+
+    /**
+     * Resent activate mail.
+     *
+     * @since 2015-11-24 00:05:29
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getResentActivateMail()
+    {
+        ;
+        if ((new User)->isLimitSendMail()) {
+            return view('user.msg')->withType('warning')->withTitle('Limited.Try again later.')->withContent("<p>Don't request too many.</p>");
+        }
+        if (parent::sendActivateMail()) {
+            return view('user.msg')->withType('success')->withTitle('Success.')->withContent("<p>Send mail success.Please check your mail.</p>");
+        } else {
+            return view('user.msg')->withType('danger')->withTitle('Failed.')->withContent("<p>Send mail failed.Try again later.</p>");
+        }
     }
 
 }
