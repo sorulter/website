bcfb80e88633ddae9c0ed5d9fd20b0880fc71944
Frank Kung <kfanjian@gmail.com>
2015-11-23 23:54:13 +0800

    add activate page controller.

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Session;
 
 class HomeController extends Controller
 {
@@ -15,6 +16,24 @@ class HomeController extends Controller
     public function getIndex()
     {
         return view('user.home');
+    }
+
+    /**
+     * Display activate page.
+     *
+     * @since 2015-11-23 23:19:55
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getActivate()
+    {
+        if (Session::get('user.activate')) {
+            return redirect('/user');
+        }
+        return view('user.msg')
+            ->withType('warning')->withTitle('Please Activate Your Account')
+            ->withContent('<p>Before you can login, you must active your account with the code sent to your email address.</p>
+            <p>If you did not receive this email, please check your junk/spam folder.Click <a href="/user/activate/resent">here</a> to resend the activation email.</p>');
     }
 
 }
