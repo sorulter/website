2e25b1ec4626d5c64eb26e637281eecade11db68
Frank Kung <kfanjian@gmail.com>
2015-11-26 00:49:25 +0800

    增加登录模板

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>iProxier | Login</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="{{ asset("/static/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="{{ asset("/static/admin-lte/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset("/static/plugins/iCheck/square/blue.css") }}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body class="hold-transition login-page layout-top-nav" style="background: url(http://ww3.sinaimg.cn/large/6816152bgw1exn0b9uqbgj21kw1230un.jpg) no-repeat center center fixed;">
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="{{ url('/') }}"><b>iProxier</b>.com</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+      <div class="alert alert-success alert-dismissable" style="display: none;" id="msg-success">
+        <button type="button" class="close" aria-hidden="true">×</button>
+        <p>...</p>
+      </div>
+      <div class="alert alert-danger alert-dismissable" style="display: none;" id="msg-error">
+        <button type="button" class="close" aria-hidden="true">×</button>
+        <p>...</p>
+      </div>
+
+      <h3>{{ $errors->default->first('email') }}</h3>
+      <h3>{{ $errors->default->first('password') }}</h3>
+      <form method="post" action="/login">
+        {!! csrf_field() !!}
+
+        <div class="form-group has-feedback">
+          <input type="text" class="form-control" placeholder="Email" name="email" id="email">
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox icheck">
+              <label>
+                <input type="checkbox" name="rememberme" id="rememberme"> Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <a href="#">I forgot my password</a>&emsp;|&emsp;<a href="#" class="text-center">Register a new membership</a>
+
+    </div>
+    <!-- /.login-box-body -->
+  </div>
+  <!-- /.login-box -->
+
+  <!-- jQuery 2.1.4 -->
+  <script src="{{ asset("/static/jquery/jquery-2.1.4.min.js") }}"></script>
+  <!-- iCheck -->
+  <script src="{{ asset("/static/plugins/iCheck/icheck.min.js") }}"></script>
+  <script>
+    $(function () {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+      });
+    });
+  </script>
+  </body>
+</html>
