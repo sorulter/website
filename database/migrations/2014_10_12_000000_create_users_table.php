ca4482816ee9275aa04a91515902bbc88f09412d
Frank Kung <kfanjian@gmail.com>
2015-11-25 23:01:09 +0800

    修改用户表结构

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration
 {
@@ -14,9 +14,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            // $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->integer('activate')->default(0); // 激活状态
+            $table->string('activate_code'); // 激活码
+            $table->float('wallet'); // 钱包
             $table->rememberToken();
             $table->timestamps();
         });
