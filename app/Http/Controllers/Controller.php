4e7329ce15d408a6bd07a267e628e0de8a30d243
Frank Kung <kfanjian@gmail.com>
2015-12-01 00:24:39 +0800

    delete base controller construct methed.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,22 +6,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Route;
-use Session;
-use View;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function __construct()
-    {
-        // global variables of view
-        View::share('user', Session::get('user'));
-        if (Route::getCurrentRoute()) {
-            list(, $action) = explode('@', Route::getCurrentRoute()->getActionName());
-            View::share('action', $action);
-        }
-    }
 
 }
