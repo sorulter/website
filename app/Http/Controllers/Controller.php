c0ec3fdc5060e83b88640d2f5f421d9e328da3f6
Frank Kung <kfanjian@gmail.com>
2015-12-01 00:23:51 +0800

    delete send activate mail method.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Mail;
 use Route;
 use Session;
 use View;
@@ -26,34 +24,4 @@ abstract class Controller extends BaseController
         }
     }
 
-    /**
-     * Send activate mail
-     *
-     * @since 2015-11-24 00:07:50
-     *
-     * return
-     */
-    public function sendActivateMail()
-    {
-        $User = new User;
-        $code = $User->activateCode();
-        if (mb_strlen($code) == 32) {
-            $data = [
-                'email' => Session::get('user.email'),
-                'code' => $code,
-                'data' => date('c'),
-            ];
-            Mail::send('emails.welcome', $data, function ($message) {
-                $email = Session::get('user.email');
-                $message->from('notifications@iProxier.com', 'iProxier Notifications');
-                $message->to($email, explode('@', $email)[0])->subject('Activate your iProxier account to start surfing! @' . date('c'));
-            });
-            $User->setLastSendMailTime(time());
-            // return response()->json(['ok' => true, 'msg' => 'Send mail success.Please check your mail.']);
-            return true;
-        } else {
-            // return response()->json(['ok' => false, 'msg' => 'Send mail failed.Try again later.']);
-            return false;
-        }
-    }
 }
