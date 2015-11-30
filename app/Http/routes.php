696555c4adb40c702fe82dae9e56d9620fca841b
Frank Kung <kfanjian@gmail.com>
2015-11-30 23:29:40 +0800

    add registration routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,3 +31,7 @@ Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+
