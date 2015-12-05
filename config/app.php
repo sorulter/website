1d29604d1279b64662a3f1cddb3ed1d56a60ba45
Frank Kung <kfanjian@gmail.com>
2015-12-05 23:34:41 +0800

    add Captcha Service Provider and aliases.

--- config/app.php
+++ config/app.php
@@ -146,6 +146,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\UserValidatorProvider::class,
 
+        Mews\Captcha\CaptchaServiceProvider::class,
+
     ],
 
     /*
@@ -194,6 +196,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
 
     ],
 
