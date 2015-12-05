9fa0aa1c1dabfdb9d2fecf70a06150d4a360599b
Frank Kung <kfanjian@gmail.com>
2015-12-06 01:23:45 +0800

    add captcha to password reset page.

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -41,6 +41,19 @@
         </div>
         <br>
 
+        <label class="control-label text-red" for="captcha" style="display: @if ($errors->default->has('captcha'))block @else none @endif;">
+          <i class="fa fa-times-circle-o"></i>
+          <span>{{ $errors->default->first('captcha') }}</span>
+        </label>
+        <div class="col-xs-5" style="padding-left: 0;">{!! captcha_img() !!}</div>
+        <div class="col-xs-7 input-group @if ($errors->default->has('captcha')) has-error @endif">
+          <input type="text" class="form-control" placeholder="Captcha" name="captcha" id="captcha">
+          <span class="input-group-addon">
+            <i class="glyphicon glyphicon-user"></i>
+          </span>
+        </div>
+        <br>
+
         <div class="row">
           <div class="col-xs-12">
             <button type="submit" class="btn btn-primary btn-block btn-float">Send Password Reset Link</button>
