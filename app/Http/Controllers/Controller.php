d51e4ecca845db52fa882951e354a5a7a2884820
Frank Kung <kfanjian@gmail.com>
2015-12-06 01:43:56 +0800

    add global view var: action.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Route;
+use View;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        list(, $action) = explode('@', Route::getCurrentRoute()->getActionName());
+        View::share('action', $action);
+    }
 
 }
