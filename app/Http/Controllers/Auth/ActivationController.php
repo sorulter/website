d9fbf1c1f2893047b8d2478ca5335bde45682070
Frank Kung <kfanjian@gmail.com>
2015-12-10 23:33:22 +0800

    Activate controller.

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
 
 class ActivationController extends Controller
 {
@@ -11,8 +13,18 @@ class ActivationController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function getActivate()
+    public function getActivate(Request $request, $token)
     {
         //
+        if (sizeof(User::where('activate_code', '=', $token)->get()) == 1) {
+            User::where('activate_code', '=', $token)->update(
+                array('activate' => 1, 'activate_code' => '')
+            );
+            return view('user.redirect')->withType('success')
+                ->withTitle('Activate Success!')
+                ->withContent('')
+                ->withTo('/login')
+                ->withTime(3);
+        }
     }
 }
