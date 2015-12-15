3aa2de564a8c5ae8989503c9254b69bc80ec219b
Frank Kung <kfanjian@gmail.com>
2015-12-16 00:01:23 +0800

    Add public header blade.

---
+++ resources/views/pub/header.blade.php
@@ -0,0 +1,15 @@
+<!-- Main Header -->
+<header class="main-header">
+
+  <!-- Logo -->
+  <a href="{{ url('/') }}" class="logo"><b>iProxier</b>.com</a>
+
+  <!-- Header Navbar -->
+  <nav class="navbar navbar-static-top2" role="navigation">
+    <!-- Navbar Right Menu -->
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+      </ul>
+    </div>
+  </nav>
+</header>
