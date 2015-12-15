40ef126340f24e10eb9abd0a8441178e513d4629
Frank Kung <kfanjian@gmail.com>
2015-12-16 00:01:53 +0800

    Add public footer blade.

---
+++ resources/views/pub/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="margin-left: 0;position: fixed; bottom: 0; width: 100%;">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2015 <a href="{{url('/')}}">iProxier.com</a>.</strong> All rights reserved.
+</footer>
