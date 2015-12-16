7a18ba3a39f7ec29c5b85be4e248b9bee3d85b7d
Frank Kung <kfanjian@gmail.com>
2015-12-17 00:27:03 +0800

    Refactoring password forgot page, use front layout.

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>iProxier</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!-- Bootstrap 3.3.2 -->
-    <link href="{{ asset("/static/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
-    <!-- Theme style -->
-    <link href="{{ asset("/static/admin-lte/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+@extends('front')
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-<body class="hold-transition login-page layout-top-nav" style="background: url(http://ww3.sinaimg.cn/large/6816152bgw1exn0b9uqbgj21kw1230un.jpg) no-repeat center center fixed;">
+@section('body-class') hold-transition login-page @stop
+
+@section('content')
   <div class="login-box">
-    <div class="login-logo">
-      <a href="{{ url('/') }}"><b>iProxier</b>.com</a>
-    </div>
-    <!-- /.login-logo -->
     <div class="login-box-body">
 
       <form method="POST" action="/forgot">
@@ -67,6 +47,4 @@
     <!-- /.login-box-body -->
   </div>
   <!-- /.login-box -->
-
-  </body>
-</html>
+@stop
