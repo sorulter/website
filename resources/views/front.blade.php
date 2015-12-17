3c78584bcb96fbc033fb082dd62a124ec3b6ae71
Frank Kung <kfanjian@gmail.com>
2015-12-18 00:52:07 +0800

    Add title yield to front layout.

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>{{ $title or "iProxier" }}</title>
+    <title>@yield('title', "Proxier") </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link href="{{ asset("/static/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
