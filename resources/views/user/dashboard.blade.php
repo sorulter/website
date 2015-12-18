1e2cfd3b8225c03b9cacce15243bbc21843b3079
Frank Kung <kfanjian@gmail.com>
2015-12-19 01:28:25 +0800

    Remove content header.

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -32,18 +32,6 @@
 
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            {{ $page_title or "Page Title" }}
-            <small>{{ $page_description or null }}</small>
-          </h1>
-          <!-- You can dynamically generate breadcrumbs here -->
-          <ol class="breadcrumb">
-            <li><a href="/#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">dashboard</li>
-          </ol>
-        </section>
 
         <!-- Main content -->
         <section class="content">
