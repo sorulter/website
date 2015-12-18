603ba474ecb788bec322098c10d75575b5859463
Frank Kung <kfanjian@gmail.com>
2015-12-19 00:56:20 +0800

    Modify sidebar.Add services, billing, settings menu.

--- resources/views/user/sidebar.blade.php
+++ resources/views/user/sidebar.blade.php
@@ -4,42 +4,35 @@
   <!-- sidebar: style can be found in sidebar.less -->
   <section class="sidebar">
 
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel">
-      <div class="pull-left image">
-        <img src="{{ $user['avatar'] or asset("/static/default/img/noavatar.png") }}" class="img-circle" alt="User Image" />
-      </div>
-      <div class="pull-left info">
-        <p>{{ $user['name'] or 'Username' }}</p>
-        <!-- Status -->
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-      </div>
-    </div>
-
-    <!-- search form (Optional) -->
-    <!--form action="#" method="get" class="sidebar-form">
-      <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search..."/>
-        <span class="input-group-btn">
-          <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-        </span>
-      </div>
-    </form-->
-    <!-- /.search form -->
-
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
       <li class="header">Menu</li>
       <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="#"><span>Link</span></a></li>
-      <li><a href="#"><span>Another Link</span></a></li>
-      <li class="treeview">
-        <a href="#"><span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
-        <ul class="treeview-menu">
-          <li><a href="#">Link in level 2</a></li>
-          <li><a href="#">Link in level 2</a></li>
-        </ul>
+      <li id="user-services" class="fa-lg">
+        <a href="/user">
+          <span class="fa-stack">
+            <i class="fa fa-user fa-stack-1x"></i>
+          </span>
+          <span>Services</span>
+        </a>
       </li>
+      <li id="user-billing" class="fa-lg">
+        <a href="/user/billing">
+          <span class="fa-stack">
+            <i class="fa fa-credit-card fa-stack-1x"></i>
+          </span>
+          <span>Billing</span>
+        </a>
+      </li>
+      <li id="user-settings" class="fa-lg">
+        <a href="/user/settings">
+          <span class="fa-stack">
+            <i class="fa fa-cog fa-stack-1x"></i>
+          </span>
+          <span>Settings</span>
+        </a>
+      </li>
+
     </ul><!-- /.sidebar-menu -->
   </section>
   <!-- /.sidebar -->
