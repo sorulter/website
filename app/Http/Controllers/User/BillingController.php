80bd4d88782d03f67d6d679de83ddda4d44f8053
Frank Kung <kfanjian@gmail.com>
2015-12-20 01:14:13 +0800

    Create new billing controller.

---
+++ app/Http/Controllers/User/BillingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+class BillingController extends Controller
+{
+    /**
+     * Display a listing of the billing.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getIndex()
+    {
+        //
+    }
+
+}
