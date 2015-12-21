ea8e85e8aae7f0e14a8756729a51ac42e446c70a
Frank Kung <kfanjian@gmail.com>
2015-12-22 00:31:15 +0800

    Modify order migrations.

--- database/migrations/2015_07_25_101746_create_orders_table.php
+++ database/migrations/2015_07_25_101746_create_orders_table.php
@@ -1,41 +1,38 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateOrdersTable extends Migration {
+class CreateOrdersTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('orders', function(Blueprint $table)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->string('state');
-            $table->integer('items_number')->unsigned();
-            $table->float('items_total', 15, 2);
-            $table->integer('adjustments_numbers')->nullable();
-            $table->float('adjustments_total', 15, 2)->nullable();
+            $table->float('total', 15, 2);
             $table->dateTime('completed_at')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-	}
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('orders');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
 
 }
