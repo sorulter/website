f4c56868ca53734a52e012a2cdbbbac473eabf30
Frank Kung <kfanjian@gmail.com>
2015-12-23 01:03:00 +0800

    Display billing index.

--- app/Http/Controllers/User/BillingController.php
+++ app/Http/Controllers/User/BillingController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Model\Order;
 
 class BillingController extends Controller
 {
     /**
-     * Display a listing of the billing.
+     * Display index of the billing.
      *
      * @return \Illuminate\Http\Response
      */
     public function getIndex()
     {
-        //
+        // get user's order.
+        $order = new Order;
+        return view('user.billing.index')->withOrders($order->where("user_id", "=", request()->user()->id)->paginate(2));
     }
 
 }
