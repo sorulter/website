ef18faaea7fcd5a4e0f071a8340486fbcefdd0bf
Frank Kung <kfanjian@gmail.com>
2015-12-23 00:39:49 +0800

    Add order model.

---
+++ app/Model/Order.php
@@ -0,0 +1,94 @@
+<?php
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+
+    protected $table = 'orders';
+
+    /**
+     * Define order state
+     * @var string
+     */
+    public $INIT = 'init';
+
+    public $OBLIGATION = 'obligation';
+
+    public $PROCESSING = 'processing';
+
+    public $COMPLETE = 'complete';
+
+    /**
+     * Create new order
+     * @param $price
+     * @param $user_id
+     * @return Order
+     */
+    public function order($price, $user_id)
+    {
+        $this->user_id = $user_id;
+        $this->state = $this->OBLIGATION;
+        $this->total = $price;
+
+        $this->save();
+        return $this;
+    }
+
+    /**
+     * Get all list
+     * @param $user_id
+     * @return mixed
+     */
+    public function getUserOrders($user_id)
+    {
+        return Order::where('user_id', $user_id)->get();
+    }
+
+    /**
+     * Change order state
+     * @param $order_id
+     * @param $status
+     * @return bool
+     */
+    public function updateStatus($order_id, $status)
+    {
+        if (!($status == $this->INIT || $status == $this->COMPLETE
+            || $status == $this->OBLIGATION || $status == $this->PROCESSING)) {
+            return false;
+        }
+
+        $order = $this->getOrder($order_id);
+
+        if (!$order) {
+            return false;
+        }
+
+        $order->state = $status;
+        $order->save();
+
+        return true;
+    }
+
+    /**
+     * Delete
+     * @param $order_id
+     * @return bool
+     */
+    public function deleteOrder($order_id)
+    {
+        $order = $this->getOrder($order_id);
+
+        if (!$order) {
+            return false;
+        }
+
+        OrderItem::where('order_id', $order->id)->delete();
+
+        $order->delete();
+
+        return true;
+    }
+
+}
