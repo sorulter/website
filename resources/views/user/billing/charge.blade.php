8a2fb590e1669434229bff4342b2c306af718a71
Frank Kung <kfanjian@gmail.com>
2015-12-24 00:31:02 +0800

    Add charge blade.

---
+++ resources/views/user/billing/charge.blade.php
@@ -0,0 +1,21 @@
+@extends('user.dashboard')
+
+@section('content')
+<section class="content-header">
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-danger">
+            <div class="box-header">
+                <h3 class="box-title">Charge <small>Alipay</small></h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
+@stop
