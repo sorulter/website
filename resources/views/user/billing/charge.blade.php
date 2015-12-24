2417c89d71529c5443846417fcbe5deff72c7da3
Frank Kung <kfanjian@gmail.com>
2015-12-25 00:07:29 +0800

    Add border to charge box header.

--- resources/views/user/billing/charge.blade.php
+++ resources/views/user/billing/charge.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-xs-12">
         <div class="box box-danger">
-            <div class="box-header">
+            <div class="box-header with-border">
                 <h3 class="box-title">Charge <small>Alipay</small></h3>
             </div>
             <!-- /.box-header -->
