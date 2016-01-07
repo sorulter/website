3aaca4ab5d8fe54911430d8d5061917ded0b2c5b
Frank Kung <kfanjian@gmail.com>
2016-01-08 00:06:55 +0800

    Add alipay gateway.

--- config/laravel-omnipay.php
+++ config/laravel-omnipay.php
@@ -7,6 +7,16 @@ return [
 
     // Add in each gateway here
     'gateways' => [
+        'alipay' => [
+            "driver" => 'Alipay_Secured',
+            'options' => [
+                'partner' => env('ALIPAY_PARTNER'),
+                'key' => env('ALIPAY_APPID'),
+                'sellerEmail' => env('ALIPAY_SELLER_EMAIL'),
+                'returnUrl' => env('ALIPAY_RETURN_URL'),
+                'notifyUrl' => env('ALIPAY_NOTIFY_URL'),
+            ],
+        ],
         'paypal' => [
             'driver' => 'PayPal_Express',
             'options' => [
