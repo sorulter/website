8479f80212dda88f9ede950c9d0527085b62ff5b
Frank Kung <kfanjian@gmail.com>
2016-01-12 00:39:26 +0800

    Add server ports sources table migration.

---
+++ database/migrations/2016_01_11_163234_create_ports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('node_doname');
+            $table->integer('port');
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pac');
+    }
+}
