a91286ce40ac192b69f7a4f74b385e505bc06208
Frank Kung <kfanjian@gmail.com>
2016-01-15 20:22:45 +0800

    Create admin auth middleware.

---
+++ app/Http/Middleware/AdminAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Verify the user's id, if not in the array, arbort.
+        if (in_array($request->user()->id, mb_split(',', env('ADMINIDS')))) {
+            app()->abort(404);
+        }
+
+        return $next($request);
+    }
+}
