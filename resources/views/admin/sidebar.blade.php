148935bfc73d8ebb768b0b3f682acbab599b0183
Frank Kung <kfanjian@gmail.com>
2016-01-15 20:40:27 +0800

    Create admin's sidebar blade.

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,39 @@
+<!-- Left side column. contains the sidebar -->
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu">
+      <li class="header">Menu</li>
+      <!-- Optionally, you can add icons to the links -->
+      <li id="user-overview" class="fa-lg">
+        <a href="/admin">
+          <span class="fa-stack">
+            <i class="fa fa-user fa-stack-1x"></i>
+          </span>
+          <span>Overview</span>
+        </a>
+      </li>
+      <li id="user-ports" class="fa-lg">
+        <a href="/admin/ports">
+          <span class="fa-stack">
+            <i class="fa fa-credit-card fa-stack-1x"></i>
+          </span>
+          <span>Ports</span>
+        </a>
+      </li>
+      <li id="user-users" class="fa-lg">
+        <a href="/admin/users">
+          <span class="fa-stack">
+            <i class="fa fa-cog fa-stack-1x"></i>
+          </span>
+          <span>Users</span>
+        </a>
+      </li>
+
+    </ul><!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
