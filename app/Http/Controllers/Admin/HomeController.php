03eede23d36aebda8b84ef9ada619841eed55d59
Frank Kung <kfanjian@gmail.com>
2016-01-15 20:55:51 +0800

    Create Admin controller and home page method.

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getIndex()
+    {
+        return view('admin.home');
+    }
+
+}
