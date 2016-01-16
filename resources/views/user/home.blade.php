95007f50dabed5fe67ee557901cbbf7addc600a5
Frank Kung <kfanjian@gmail.com>
2016-01-17 00:44:43 +0800

    Two column.

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 <div class='row'>
   <div class="col-md-12">
+    <div class="col-md-6">
+    </div>
+
+    <div class="col-md-6">
+    </div>
 
   </div>
 </div><!-- /.row -->
