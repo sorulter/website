69a1fa81b72bdeead8efeb6f0d9b1e56de70ede7
Frank Kung <kfanjian@gmail.com>
2016-01-16 22:05:12 +0800

    Create admin's user list blade.

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.dashboard')
+
+@section('content')
+<div class='row'>
+  <div class="col-md-12">
+
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Ports List</h3>
+            <div class="box-tools">
+                <a href="{{route('admin/ports')}}"><span class="label label-success">All</span></a>
+                <a href="{{route('admin/ports/index/used')}}"><span class="label label-success">Used</span></a>
+                <a href="{{route('admin/ports/index/empty')}}"><span class="label label-success">Empty</span></a>
+
+                <a href="{{route('admin/ports/add')}}"><span class="label label-info">Add Ports</span></a>
+            </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th style="width: 10px">#</th>
+                        <th>Email</th>
+                        <th>Activate</th>
+                        <th>Created</th>
+                        <th>Updated</th>
+                        <th>Actions</th>
+                    </tr>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->activate}}</td>
+                        <td>{{$user->created_at}}</td>
+                        <td>{{$user->updated_at}}</td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer clearfix">
+        	@include('pagination.large', ['paginator' => $users])
+        </div>
+    </div>
+
+  </div>
+</div><!-- /.row -->
+@endsection
