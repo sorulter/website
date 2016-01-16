ef3e11f2b1a4e5ce2c62d9efce5f2a3781b89d49
Frank Kung <kfanjian@gmail.com>
2016-01-17 00:45:54 +0800

    Add panel "Free to Try".

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -4,6 +4,26 @@
 <div class='row'>
   <div class="col-md-12">
     <div class="col-md-6">
+
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">Free to Try</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <p> Welcome to use IPX(iPorxier).There is the guide for you.</p>
+            </div>
+            <!-- /.box-body -->
+
+            <div class="box-footer no-padding" style="display: block;">
+            </div>
+            <!-- /.footer -->
+        </div>
+
     </div>
 
     <div class="col-md-6">
