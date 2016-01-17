14a183687063b924024e7b6101d23cb9ec2c7b4e
Frank Kung <kfanjian@gmail.com>
2016-01-17 22:58:02 +0800

    Create settings index method.

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -6,4 +6,13 @@ use App\Http\Controllers\Controller;
 
 class SettingsController extends Controller
 {
+    /**
+     * Display settings index.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getIndex()
+    {
+        return view('user.settings.index');
+    }
 }
