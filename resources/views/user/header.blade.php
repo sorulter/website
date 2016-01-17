5289c656ff717e4aedad31fe02d6e1b0dfd5b230
Frank Kung <kfanjian@gmail.com>
2016-01-17 23:05:33 +0800

    Rename profile to settings and change link.

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -45,7 +45,7 @@
             <!-- Menu Footer-->
             <li class="user-footer">
               <div class="pull-left">
-                <a href="{{ url('/user/profile') }}" class="btn btn-default btn-flat">Profile</a>
+                <a href="{{ url('/user/settings') }}" class="btn btn-default btn-flat">Settings</a>
               </div>
               <div class="pull-right">
                 <a href="{{ url('/logout') }}" class="btn btn-default btn-flat">Sign out</a>
