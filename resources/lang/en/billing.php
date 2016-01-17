85f081eccecd9561dd3b3a24af7d2228ae71d4d7
Frank Kung <kfanjian@gmail.com>
2016-01-17 16:27:24 +0800

    Create billing lang file.

---
+++ resources/lang/en/billing.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'Order ID' => 'Order ID',
+    'Billing History' => 'Billing History',
+    'Amount' => 'Amount',
+    'State' => 'State',
+    'Created At' => 'Created At',
+    'Details' => 'Details',
+];
