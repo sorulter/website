71db1e4e6b03eb2c271f60965f4b3d3ef1e476b2
Frank Kung <kfanjian@gmail.com>
2016-01-20 01:39:39 +0800

    Rename mobile config link.

--- resources/views/user/cellular/index.blade.php
+++ resources/views/user/cellular/index.blade.php
@@ -15,42 +15,42 @@
         <div class="box-body">
             <div class="row">
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'cmcc')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', 'cmnet')}}" target="_blank">
                         <img src="/static/images/icon/CMCC.png" alt="control" class="img-responsive">
                         <span><h4>CMNET</h4></span>
                     </a>
                 </div>
 
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'telcom')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', 'ctnet')}}" target="_blank">
                         <img src="/static/images/icon/TELCOM.png" alt="control" class="img-responsive">
                         <span><h4>CTNET</h4></span>
                     </a>
                 </div>
 
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'unicom')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', '3gnet')}}" target="_blank">
                         <img src="/static/images/icon/UNICOM.png" alt="control" class="img-responsive">
                         <span><h4>3GNET</h4></span>
                     </a>
                 </div>
 
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'cmcc')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', 'cmwap')}}" target="_blank">
                         <img src="/static/images/icon/CMCC.png" alt="control" class="img-responsive">
                         <span><h4>CMWAP</h4></span>
                     </a>
                 </div>
 
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'telcom')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', 'ctwap')}}" target="_blank">
                         <img src="/static/images/icon/TELCOM.png" alt="control" class="img-responsive">
                         <span><h4>CTWAP</h4></span>
                     </a>
                 </div>
 
                 <div class="col-sm-4 text-center">
-                    <a href="{{route('user/cellular/config', 'unicom')}}" target="_blank">
+                    <a href="{{route('user/cellular/config', 'uninet')}}" target="_blank">
                         <img src="/static/images/icon/UNICOM.png" alt="control" class="img-responsive">
                         <span><h4>UNINET</h4></span>
                     </a>
