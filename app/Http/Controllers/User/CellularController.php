19d5939dd39f350fe4c7a8b707ad232f861155d3
Frank Kung <kfanjian@gmail.com>
2016-01-19 21:39:02 +0800

    Just serve for user that use iPhone's Safari browser.

--- app/Http/Controllers/User/CellularController.php
+++ app/Http/Controllers/User/CellularController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers\User;
 
+use Agent;
 use App\Http\Controllers\Controller;
 
 class CellularController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        if (!(Agent::is('iPhone') && Agent::is('Safari'))) {
+            return abort(404);
+        }
+    }
 }
