a4f1624435757d60de82446db6e9c81a7ed2d7b1
Frank Kung <kfanjian@gmail.com>
2016-01-20 22:16:48 +0800

    Create APN mobile config template.

---
+++ resources/views/user/cellular/disableAPN.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>PayloadContent</key>
+	<array>
+		<dict>
+			<key>PayloadContent</key>
+			<array>
+				<dict>
+					<key>DefaultsData</key>
+					<dict>
+						<key>apns</key>
+						<array>
+							<dict>
+								<key>apn</key>
+								<string>{{ $apnName }}</string>
+								<key>proxy</key>
+								<string></string>
+								<key>proxyPort</key>
+								<integer>0</integer>
+								<key>username</key>
+								<string></string>
+							</dict>
+						</array>
+					</dict>
+					<key>DefaultsDomainName</key>
+					<string>com.apple.managedCarrier</string>
+				</dict>
+			</array>
+			<key>PayloadDescription</key>
+			<string></string>
+			<key>PayloadDisplayName</key>
+			<string>APN</string>
+			<key>PayloadIdentifier</key>
+			<string>iProxier.com.apn</string>
+			<key>PayloadOrganization</key>
+			<string></string>
+			<key>PayloadType</key>
+			<string>com.apple.apn.managed</string>
+			<key>PayloadUUID</key>
+			<string>{{ $apnUUID }}</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+		</dict>
+	</array>
+	<key>PayloadDescription</key>
+	<string> Cellular Data Profile｜取消IPX移动网络配置</string>
+	<key>PayloadDisplayName</key>
+	<string>IPX＠iProxier.com</string>
+	<key>PayloadIdentifier</key>
+	<string>www.iProxier.com</string>
+	<key>PayloadOrganization</key>
+	<string>www.iProxier.com</string>
+	<key>PayloadRemovalDisallowed</key>
+	<false/>
+	<key>PayloadType</key>
+	<string>Configuration</string>
+	<key>PayloadUUID</key>
+	<string>{{ $configUUID }}</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</plist>
