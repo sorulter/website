767f66bf1fb94eab7b540c5f81e0085ec030e28e
Frank Kung <kfanjian@gmail.com>
2016-01-23 17:50:11 +0800

    Create route rules and methods to send message mail.

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SendMsgEmail;
 use App\User;
 
 class UsersController extends Controller
@@ -30,6 +31,31 @@ class UsersController extends Controller
             $msg = $user->activate();
         }
         return redirect()->back()->with('msg', $msg);
+    }
+
+    /**
+     * Send mail to user.
+     *
+     */
+    public function getSendMail($id)
+    {
+        return view('admin.users.sendmail')->with('toUser', User::find($id));
+    }
+
+    /**
+     * Send mail.
+     *
+     */
+    public function postSendMail($id)
+    {
+        $this->validate(request(), [
+            'title' => 'required',
+            'msg' => 'required',
+        ]);
+
+        $job = (new SendMsgEmail(request()->get('title'), request()->get('msg'), User::find($id)))->delay(10);
+        $this->dispatch($job);
+        return redirect()->back()->with('msg', 'send mail success.');
     }
 
 }
