e55d1ce859246c427f492c9df192df9632b63642
Frank Kung <kfanjian@gmail.com>
2016-01-26 23:40:37 +0800

    Create method to display categories.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -14,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = new Category;
+        return view('admin.articles.categories_list')->withCategories($categories->paginate(env('PERPAGE')));
     }
 
 }
