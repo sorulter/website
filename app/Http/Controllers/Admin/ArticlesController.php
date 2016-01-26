13a4aba777d406ccdb612925b07fc8545343ecf0
Frank Kung <kfanjian@gmail.com>
2016-01-27 00:53:29 +0800

    Create method of articles list.

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -3,7 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Model\Articles;
 
 class ArticlesController extends Controller
 {
+    /**
+     * Display a listing of the artilces.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = new Articles;
+        return view('admin.articles.index')->withArticles($articles->paginate(env('PERPAGE')));
+    }
 }
