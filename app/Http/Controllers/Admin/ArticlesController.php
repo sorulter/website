49efdd72c88fdb6ea4a19df501217f60b38fad6e
Frank Kung <kfanjian@gmail.com>
2016-01-27 00:55:02 +0800

    Create empty methods of articles.

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Model\Articles;
+use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
@@ -16,5 +17,65 @@ class ArticlesController extends Controller
     {
         $articles = new Articles;
         return view('admin.articles.index')->withArticles($articles->paginate(env('PERPAGE')));
+    }
+
+    /**
+     * Show the form for creating a new articles.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created articles in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified articles.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified articles.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+    }
+
+    /**
+     * Update the specified articles in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+    }
+
+    /**
+     * Remove the specified articles from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
     }
 }
