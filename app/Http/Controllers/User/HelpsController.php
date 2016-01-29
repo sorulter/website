d42f41bb8a414e269373458201e47e8c9713f0d7
Frank Kung <kfanjian@gmail.com>
2016-01-30 01:09:53 +0800

    Create method to show helps list.

--- app/Http/Controllers/User/HelpsController.php
+++ app/Http/Controllers/User/HelpsController.php
@@ -3,7 +3,20 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Model\Articles;
 
 class HelpsController extends Controller
 {
+    /**
+     * Display a listing of the helps.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $helps = new Articles;
+        return view('user.helps.index')->withHelps($helps->where('category_id', '=', env('HELPS_CAT_ID'))->paginate(env('PERPAGE')));
+
+    }
+
 }
