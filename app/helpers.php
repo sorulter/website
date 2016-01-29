42cef0610ed168a6228c640517b426feac080636
Frank Kung <kfanjian@gmail.com>
2016-01-29 21:59:22 +0800

    Create shortcode helper function.

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,17 @@ function id2hash($id)
 {
     return Hashids::encode($id);
 }
+
+function shortcode($content = null)
+{
+    $user = request()->user();
+    $pac = env('PAC_BASE_URL') . id2hash($user->id);
+    $server = $user->port->node_name . env('NODE_BASE_NAME');
+    $port = $user->port->port;
+
+    $content = mb_ereg_replace("\[pac\]", $pac, $content);
+    $content = mb_ereg_replace("\[server\]", $server, $content);
+    $content = mb_ereg_replace("\[port\]", $port, $content);
+
+    return $content;
+}
