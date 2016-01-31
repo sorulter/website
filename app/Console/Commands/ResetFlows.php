abaa6ba0518c96c6a3f0b044598a8bb33181c6ff
Frank Kung <kfanjian@gmail.com>
2016-01-31 14:14:07 +0800

    Chunk when reset flows of users.

--- app/Console/Commands/ResetFlows.php
+++ app/Console/Commands/ResetFlows.php
@@ -44,45 +44,46 @@ class ResetFlows extends Command
         $first = Carbon::today(config()->get('app.timezone'))->firstOfMonth();
 
         // Only can be run at first day of the month.
-        // if ($today->ne($first)) {
-        //     Log::warning('Only can be run at first day of the month.');
-        //     return;
-        // }
-
-        $flows = Flows::all();
-        Log::info("user flows info backup: {$flows}");
-
-        foreach ($flows as $flow) {
-
-            $overflow = $flow->used - ($flow->free + $flow->combo_flows);
-            $raw = clone $flow;
-
-            // 1. Enough or overflow.
-            if ($overflow >= 0) {
-                $flow->used = $overflow;
-                $flow->free = 0;
-            }
-
-            // 2. Not used all,but combo is enough or overflow.
-            if ($overflow < 0 && $raw->used >= $raw->combo_flows) {
-                $flow->free = $flow->free - $flow->used + $flow->combo_flows;
-                $flow->used = 0;
-            }
-
-            // 3. Combo is not used all.
-            if ($overflow < 0 && $raw->used < $raw->combo_flows) {
-                $flow->used = 0;
-            }
-
-            // User no combo flows.
-            if ($flow->combo_end_date < $today) {
-                exec("say 'user no combo flows.'");
-                $flow->combo_flows = 0;
-            }
-
-            // Store to db.
-            $flow->save();
+        if ($today->ne($first)) {
+            Log::warning('Only can be run at first day of the month.');
+            return;
         }
+
+        Flows::chunk(100, function ($flows) use ($today) {
+            Log::info("user flows info backup: {$flows}");
+
+            foreach ($flows as $flow) {
+
+                $overflow = $flow->used - ($flow->free + $flow->combo_flows);
+                $raw = clone $flow;
+
+                // 1. Enough or overflow.
+                if ($overflow >= 0) {
+                    $flow->used = $overflow;
+                    $flow->free = 0;
+                }
+
+                // 2. Not used all,but combo is enough or overflow.
+                if ($overflow < 0 && $raw->used >= $raw->combo_flows) {
+                    $flow->free = $flow->free - $flow->used + $flow->combo_flows;
+                    $flow->used = 0;
+                }
+
+                // 3. Combo is not used all.
+                if ($overflow < 0 && $raw->used < $raw->combo_flows) {
+                    $flow->used = 0;
+                }
+
+                // User no combo flows.
+                if ($flow->combo_end_date < $today) {
+                    $flow->combo_flows = 0;
+                }
+
+                // Store to db.
+                $flow->save();
+            }
+        });
+
         Log::info('Have reset flows of all users.');
     }
 }
