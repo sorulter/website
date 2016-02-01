05ec740a3d9b75c9a0c8fa6058798eb2a1dc59d0
Frank Kung <kfanjian@gmail.com>
2016-02-01 22:37:53 +0800

    Modify default queue driver.

--- config/queue.php
+++ config/queue.php
@@ -14,9 +14,9 @@ return [
     | Supported: "null", "sync", "database", "beanstalkd",
     |            "sqs", "iron", "redis"
     |
-    */
+     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('QUEUE_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -27,7 +27,7 @@ return [
     | is used by your application. A default configuration has been added
     | for each back-end shipped with Laravel. You are free to add more.
     |
-    */
+     */
 
     'connections' => [
 
@@ -44,32 +44,32 @@ return [
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
-            'host'   => 'localhost',
-            'queue'  => 'default',
-            'ttr'    => 60,
+            'host' => 'localhost',
+            'queue' => 'default',
+            'ttr' => 60,
         ],
 
         'sqs' => [
             'driver' => 'sqs',
-            'key'    => 'your-public-key',
+            'key' => 'your-public-key',
             'secret' => 'your-secret-key',
-            'queue'  => 'your-queue-url',
+            'queue' => 'your-queue-url',
             'region' => 'us-east-1',
         ],
 
         'iron' => [
-            'driver'  => 'iron',
-            'host'    => 'mq-aws-us-east-1.iron.io',
-            'token'   => 'your-token',
+            'driver' => 'iron',
+            'host' => 'mq-aws-us-east-1.iron.io',
+            'token' => 'your-token',
             'project' => 'your-project-id',
-            'queue'   => 'your-queue-name',
+            'queue' => 'your-queue-name',
             'encrypt' => true,
         ],
 
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue'  => 'default',
+            'queue' => 'default',
             'expire' => 60,
         ],
 
@@ -84,10 +84,10 @@ return [
     | can control which database and table are used to store the jobs that
     | have failed. You may change them to any database / table you wish.
     |
-    */
+     */
 
     'failed' => [
-        'database'  => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'failed_jobs',
     ],
 
