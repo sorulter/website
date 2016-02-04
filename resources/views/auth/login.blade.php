757e69f7d79f5f616e232f67a36fe6c02fdc638d
Frank Kung <kfanjian@gmail.com>
2016-02-04 22:27:58 +0800

    Translate login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('body-class') hold-transition login-page @stop
 @section('title')
-iProxier › Login
+iProxier › {{ trans('base.login') }}
 @stop
 
 @section('content')
@@ -10,7 +10,7 @@ iProxier › Login
 <link rel="stylesheet" href="{{ asset("/static/plugins/iCheck/square/blue.css") }}">
   <div class="login-box">
     <div class="login-box-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">{{ trans('base.login_subtitle') }}</p>
 
       <form method="post" action="/login">
         {!! csrf_field() !!}
@@ -20,30 +20,30 @@ iProxier › Login
           <span>{{ $errors->default->first('email') }}</span>
         </label>
         <div class="form-group has-feedback @if ($errors->default->has('email')) has-error @endif">
-          <input type="text" class="form-control" placeholder="Email" name="email" id="email" value="{{ old('email') }}">
+          <input type="text" class="form-control" placeholder="{{ trans('base.email') }}" name="email" id="email" value="{{ old('email') }}">
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-          <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+          <input type="password" class="form-control" placeholder="{{ trans('base.password') }}" name="password" id="password">
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
           <div class="col-xs-8">
             <div class="checkbox icheck">
               <label>
-                <input type="checkbox" name="rememberme" id="rememberme"> Remember Me
+                <input type="checkbox" name="rememberme" id="rememberme"> {{ trans('base.rememberme') }}
               </label>
             </div>
           </div>
           <!-- /.col -->
           <div class="col-xs-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('base.signin') }}</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
-      <a href="/forgot">I forgot my password</a>&emsp;|&emsp;<a href="/register" class="text-center">Register a new membership</a>
+      <a href="/forgot">{{ trans('base.forget_password') }}</a>&emsp;|&emsp;<a href="/register" class="text-center">{{ trans('base.regsiter_account') }}</a>
 
     </div>
     <!-- /.login-box-body -->
