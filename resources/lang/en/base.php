d8135e4cfc22aa8e3914c3d8f74d3a6436a77a65
Frank Kung <kfanjian@gmail.com>
2016-02-04 22:27:24 +0800

    Create base language lines.

---
+++ resources/lang/en/base.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'login' => 'Login',
+    'email' => 'Email',
+    'password' => 'Password',
+    'rememberme' => 'Remember Me',
+    'signin' => 'Sign In',
+    'forget_password' => 'I forgot my password',
+    'regsiter_account' => 'Register a new membership',
+    'login_subtitle' => 'Sign in to start your session',
+
+];
