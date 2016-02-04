01c27124c0a76055d806ac4eaddcb8eca71a8863
Frank Kung <kfanjian@gmail.com>
2016-02-04 22:47:42 +0800

    Add language lines of password reset page.

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -12,4 +12,7 @@ return [
     'captcha' => 'Captcha',
     'reset_via_email' => 'Reset your password via email',
     'send_reset_mail' => 'Send Password Reset Link',
+
+    'reset_password' => 'Reset your password',
+    'submit' => 'Submit',
 ];
