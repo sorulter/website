6a8a1f67bddb31fdb10e04616a94a0dcc1ea5d74
Frank Kung <kfanjian@gmail.com>
2016-02-05 06:53:10 +0800

    Create gift page post method.

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Model\Flows;
+use App\Model\Order;
 use App\User;
 use Mail;
 use Swift_Mailer;
@@ -98,6 +100,34 @@ class UsersController extends Controller
     public function getGift($id)
     {
         return view('admin.users.gift')->with('toUser', User::find($id));
+    }
+
+    public function postGift($id)
+    {
+        $this->validate(request(), [
+            'flows' => 'required',
+        ]);
+        $to = User::find($id);
+        $flows = request()->flows;
+        $title = trans('base.gift_to_user', ['flows' => $flows]);
+        $msg = trans('base.gift_msg', ['flows' => $flows, 'email' => $to->email]);
+
+        // charge free flows to try.
+        $order = new Order;
+        $order->order(0, $to->id);
+        $order->state = 'TRADE_FINISHED';
+        $order->save();
+
+        $flowsModel = Flows::where('user_id', '=', $to->id)->first();
+        $flowsModel->free += $flows * MB;
+        $flowsModel->save();
+
+        Mail::laterOn('default', 10, 'emails.msg', ['user' => $to, 'title' => $title, 'msg' => $msg], function ($m) use ($to, $title) {
+            $m->to($to->email)->subject($title);
+        });
+
+        return redirect()->back()->with('msg', 'gift flows success.');
+
     }
 
 }
