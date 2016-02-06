3ae1b6a26607a4c4a753f207d9fc77e892e44aed
Frank Kung <kfanjian@gmail.com>
2016-02-06 19:28:16 +0800

    Modify table to responsive.

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <!-- /.box-header -->
-            <div class="box-body">
+            <div class="box-body table-responsive no-padding">
                 <table class="table table-striped">
                     <tbody>
                         <tr>
