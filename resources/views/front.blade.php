6cd76d3dde03870071d92bf6b7ec6e00c6f31579
Frank Kung <kfanjian@gmail.com>
2016-02-09 00:37:54 +0800

    Replace assets url with CDN_BASE dot config field.

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -5,12 +5,12 @@
     <title>@yield('title', "Proxier") </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
-    <link href="{{ asset("/static/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ env('CDN_BASE') }}/static/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="{{ asset("/static/admin-lte/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset("/static/admin-lte/css/skins/skin-blue.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{ env('CDN_BASE') }}/static/admin-lte/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ env('CDN_BASE') }}/static/admin-lte/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css" />
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,11 +39,11 @@
     <!-- REQUIRED JS SCRIPTS -->
 
     <!-- jQuery 2.1.4 -->
-    <script src="{{ asset ("/static/jquery/jquery-2.1.4.min.js") }}"></script>
+    <script src="{{ env('CDN_BASE') }}/static/jquery/jquery-2.1.4.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
-    <script src="{{ asset ("/static/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+    <script src="{{ env('CDN_BASE') }}/static/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset ("/static/admin-lte/js/app.min.js") }}" type="text/javascript"></script>
+    <script src="{{ env('CDN_BASE') }}/static/admin-lte/js/app.min.js" type="text/javascript"></script>
     @yield('js')
   </body>
 </html>
