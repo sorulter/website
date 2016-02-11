0e794cf91cbd3fbbc312971edd04bb7b652cf4dd
Frank Kung <kfanjian@gmail.com>
2016-02-11 13:11:05 +0800

    Create api_tokens migration.

---
+++ database/migrations/2016_02_11_115229_create_api_token_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateApiTokenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_tokens', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->string('token');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('api_tokens');
+    }
+}
