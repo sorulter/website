de18e3060b0dd693d4d9a1e536ef2af6c06744e2
Frank Kung <kfanjian@gmail.com>
2016-02-14 16:15:07 +0800

    Create method to login and return api token if success.

--- app/Http/Controllers/API/IndexController.php
+++ app/Http/Controllers/API/IndexController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Model\ApiToken;
+use Auth;
+use Illuminate\Support\Str;
 
 class IndexController extends Controller
 {
+
+    /**
+     * login and return user data.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        $api = (object) [
+            'status' => 200,
+            'message' => 'login success.',
+            'data' => null,
+        ];
+
+        if (Auth::attempt(['email' => request()->email, 'password' => request()->password])) {
+            $user = Auth::user();
+
+            // Generate token and store.
+            $token = ApiToken::find($user->id);
+            if ($token == null) {
+                $token = new ApiToken;
+            }
+            $token->user_id = $user->id;
+            $token->token = Str::random(60);
+            $token->save();
+
+            $api->data = $token->token;
+        } else {
+            $api->status = 404;
+            $api->message = 'email or password error.';
+        }
+
+        return response()->json($api);
+    }
 
 }
