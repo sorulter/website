69b89a7fd4fc44e3ada8226ea743883183854457
Frank Kung <kfanjian@gmail.com>
2016-02-15 21:48:51 +0800

    Create method to return account info.

--- app/Http/Controllers/API/IndexController.php
+++ app/Http/Controllers/API/IndexController.php
@@ -44,4 +44,36 @@ class IndexController extends BaseController
         return response()->json($api);
     }
 
+    /**
+     * show account info.
+     *
+     * return \Illuminate\Http\Respone
+     */
+    public function account()
+    {
+        $api = (object) [
+            'status' => 200,
+            'message' => 'success.',
+            'data' => null,
+        ];
+
+        if ($this->user == null) {
+            $api->status = 500;
+            $api->message = "invalid token.";
+        } else {
+            $data = [
+                'id' => App\id2hash($this->user->id),
+                'email' => $this->user->email,
+                'node' => $this->user->port->node_name,
+                'port' => $this->user->port->port,
+                'used' => $this->user->flows->used,
+                'free' => $this->user->flows->free,
+                'combo_flows' => $this->user->flows->combo_flows,
+                'combo_end_date' => $this->user->flows->combo_end_date,
+            ];
+            $api->data = (object) $data;
+        }
+        return response()->json($api);
+    }
+
 }
