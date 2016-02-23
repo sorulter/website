6fd8ec4fa63f28a2d4d0109888f04e22a7d44c7c
Frank Kung <kfanjian@gmail.com>
2016-02-23 22:57:10 +0800

    Add support box to user's dashboard.

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -117,6 +117,24 @@
     <div class="col-md-6">
         <div class="box box-info">
             <div class="box-header with-border">
+                <h3 class="box-title">Support</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+            </div>
+            <!-- /.box-body -->
+
+            <div class="box-footer no-padding" style="display: block;">
+            </div>
+            <!-- /.footer -->
+        </div>
+
+        <div class="box box-info">
+            <div class="box-header with-border">
                 <h3 class="box-title">How to Configure</h3>
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
