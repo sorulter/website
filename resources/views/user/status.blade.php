4de602231ef9e0a70a56010999d68a8fe10c645e
Frank Kung <kfanjian@gmail.com>
2016-03-03 22:19:19 +0800

    Modify title of status page.

--- resources/views/user/status.blade.php
+++ resources/views/user/status.blade.php
@@ -5,7 +5,7 @@
   <div class="col-md-12">
     <div class="box box-info">
         <div class="box-header with-border">
-            <h3 class="box-title">Status Health Checker</h3>
+            <h3 class="box-title">Status</h3>
             <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                 </button>
