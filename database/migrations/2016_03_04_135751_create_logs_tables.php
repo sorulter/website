d896b15a1e7e7694a29aeee5b8a26b4a785e1953
Frank Kung <kfanjian@gmail.com>
2016-03-04 23:31:25 +0800

    Create multi logs tables.

---
+++ database/migrations/2016_03_04_135751_create_logs_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTables extends Migration
+{
+    private $tables_suffix = ['a', 'b', 'c', 'd', 'e'];
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->tables_suffix as $suffix) {
+            Schema::create('logs_' . $suffix, function ($table) {
+                $table->integer('user_id')->unsigned();
+                $table->integer('flows');
+                $table->integer('client_ip');
+                $table->timestamp('used_at');
+
+                $table->foreign('user_id')->references('id')->on('users');
+            });
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->tables_suffix as $suffix) {
+            Schema::drop('logs_' . $suffix);
+        }
+    }
+}
