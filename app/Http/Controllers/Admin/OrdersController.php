ffbc345a2aa4ebbc33208c44a167ca85eb6660df
Frank Kung <kfanjian@gmail.com>
2016-03-14 23:20:11 +0800

    Create paid method.

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -18,4 +18,15 @@ class OrdersController extends Controller
         return view('admin.orders.index')->withOrders($orders->orderBy('id', 'DESC')->paginate(env('PERPAGE')));
     }
 
+    /**
+     * Display a listing of paid orders.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function paid()
+    {
+        $orders = new Order;
+        return view('admin.orders.index')->withOrders($orders->where('amount', '>', 0)->where('state', '=', 'TRADE_FINISHED')->orderBy('id', 'DESC')->paginate(env('PERPAGE')));
+    }
+
 }
