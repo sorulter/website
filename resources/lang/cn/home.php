ddc1c244f5f2fcbde2810d95edb8697a807b7b6c
Frank Kung <kfanjian@gmail.com>
2016-03-18 00:01:16 +0800

    Translate user's homepage to Chinese.

---
+++ resources/lang/cn/home.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'account_info' => '帐户信息',
+    'free_flows' => '永久流量',
+    'combo_flows' => '套餐流量',
+    'used_flows' => '已经使用',
+    'front_end' => '接入点',
+    'combo_end_date' => '套餐截至',
+    'proxy_server' => '代理地址',
+    'proxy_port' => '代理端口',
+    'pac_url' => 'Pac 网址',
+    'no_combo' => '无套餐.请订购!',
+    'support' => '技术支持',
+    'how_to_configure' => '如何配置',
+];
