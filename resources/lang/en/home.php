c289d91b416b40283d0d4d959525d30e7ad0b9e7
Frank Kung <kfanjian@gmail.com>
2016-03-17 22:02:54 +0800

    Create lang file for homepage.

---
+++ resources/lang/en/home.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'account_info' => 'Account Info',
+    'free_flows' => 'Free Flows',
+    'combo_flows' => 'Combo Flows',
+    'used_flows' => 'Used Flows',
+    'front_end' => 'Front End',
+    'combo_end_date' => 'Combo End Date',
+    'proxy_server' => 'Proxy Server',
+    'proxy_port' => 'Proxy Port',
+    'pac_url' => 'Pac URL',
+    'no_combo' => 'No combo flows.Order now!',
+    'support' => 'Support',
+    'how_to_configure' => 'How to Configure',
+];
