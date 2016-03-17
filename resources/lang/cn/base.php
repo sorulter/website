4a6c4a046dce8b6cb0b2145285c11ff908b1f677
Frank Kung <kfanjian@gmail.com>
2016-03-18 00:01:55 +0800

    Translate base msg to Chinese.

---
+++ resources/lang/cn/base.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    'login' => '登录',
+    'email' => '邮箱',
+    'password' => '密码',
+    'rememberme' => '记住登录',
+    'signin' => '登录',
+    'forget_password' => '忘记密码',
+    'regsiter_account' => '注册新帐号',
+    'login_subtitle' => '开启新的世界',
+
+    'captcha' => '验证码',
+    'reset_via_email' => '通过邮件找回密码',
+    'send_reset_mail' => '密码重置邮件已发送',
+
+    'reset_password' => '重置密码',
+    'submit' => '提交',
+
+    'gift_to_user' => '赠送您 :flows MB 流量.',
+    'gift_msg' => '尊敬的用户, 我们赠送给您了 :flows MB 流量到您的帐号 :email',
+
+    'blocked' => '帐号被封禁.',
+    'break_the_eual' => '您违反了最终用户协议或者有违反当地法律法规的行为，请及时终止您的不当行为.',
+
+    // sidebar
+    'menu' => '菜单',
+    'services' => '用户首页',
+    'billing' => '账单信息',
+    'helps' => '帮助信息',
+    'logs' => '日志记录',
+    'settings' => '系统设置',
+    // header & footer
+    'status' => '状态',
+    'charge' => '购买',
+    'sign_out' => '登出',
+    'all_rights_reserved' => '保留最终解释权.',
+
+    'need_activate_title' => '务必激活!',
+    'need_activate' => '请先激活您的帐号.',
+
+];
