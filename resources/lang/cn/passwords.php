e17a44ca657078642bbff8c974c13f5f1f7fd702
Frank Kung <kfanjian@gmail.com>
2016-03-21 00:50:23 +0800

    Create translate lines for passwords.

---
+++ resources/lang/cn/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+     */
+
+    'reset' => '重置密码成功!',
+    'sent' => '密码重置链接已发送到您的邮箱!',
+    'token' => '密码重置令牌无效.',
+    'user' => "此邮箱不存在.",
+
+];
