6fb386c7d1e5ea547312b223be7f0185bde38e22
Frank Kung <kfanjian@gmail.com>
2016-04-01 00:39:58 +0800

    Allow soft delete products.

--- app/Model/Products.php
+++ app/Model/Products.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Products extends Model
 {
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
     protected $fillable = array('name', 'price', 'type', 'amount', 'describe');
 }
