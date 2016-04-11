7d12df86047670d5f2b682d0161cb2c7628a8ca1
Frank Kung <kfanjian@gmail.com>
2016-04-12 06:26:48 +0800

    Set referrer and ad_source fields nullable.

--- database/migrations/2016_04_11_091630_add_user_count_fields.php
+++ database/migrations/2016_04_11_091630_add_user_count_fields.php
@@ -13,9 +13,9 @@ class AddUserCountFields extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('ad_source')->after('remember_token');
+            $table->string('ad_source')->nullable()->after('remember_token');
             $table->integer('invite_id')->after('ad_source');
-            $table->string('referrer')->after('invite_id');
+            $table->string('referrer')->nullable()->after('invite_id');
         });
     }
 
