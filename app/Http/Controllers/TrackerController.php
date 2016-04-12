056c9520d850e84b7366301be6bfb206d2dc0a32
Frank Kung <kfanjian@gmail.com>
2016-04-12 21:23:39 +0800

    Save ad track.

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Model\Track;
+use Carbon\Carbon;
 
 class TrackerController extends Controller
 {
@@ -13,6 +15,21 @@ class TrackerController extends Controller
      */
     public function ad($name)
     {
+        $tracker = Track::firstOrNew([
+            'day' => Carbon::now()->toDateString(),
+            'type' => 'ad',
+            'source' => $name,
+            'ip' => request()->ip(),
+        ]);
+
+        $tracker->day = Carbon::now()->toDateString();
+        $tracker->type = 'ad';
+        $tracker->source = $name;
+        $tracker->source = $name;
+        $tracker->ip = request()->ip();
+        $tracker->count += 1;
+        $tracker->save();
+
         session(['ad_source' => $name]);
 
         return redirect()->route('register');
