1310c0129222559a3ecae3b0d077d58a75f8698a
Frank Kung <kfanjian@gmail.com>
2016-04-16 23:21:49 +0800

    Add content header to show title and subtitle for user.

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -31,6 +31,11 @@
 
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
+        <!-- Content header -->
+        <section class="content-header">
+            <h1>@if (isset($title)) {{$title}} @endif
+            <small>@if (isset($subtitle)) {{$subtitle}} @endif</small></h1>
+        </section>
 
         <!-- Main content -->
         <section class="content">
