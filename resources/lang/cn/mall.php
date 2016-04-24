f44e9ca2a120a4671ca4428182ff3e523bed91b1
Frank Kung <kfanjian@gmail.com>
2016-04-25 00:29:28 +0800

    Translate.

--- resources/lang/cn/mall.php
+++ resources/lang/cn/mall.php
@@ -2,5 +2,15 @@
 return [
     'title' => 'iProxier 流量',
     'combo_flows' => '包年包月',
+    'products' => '产品',
     'forever_flows' => '按量付费',
+    'flows_per_month' => '每月流量',
+    'duration' => '购买时长',
+    'years' => '年',
+    'months' => '个月',
+    'fees' => '费用',
+    'orig' => '原价',
+    'discount' => '优惠',
+    'subtotal' => '应付',
+    'pay' => '立即购买',
 ];
