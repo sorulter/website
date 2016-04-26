8392756e9ffe11ebc92dd5f8b7a1cc754f2e6524
Frank Kung <kfanjian@gmail.com>
2016-04-26 22:22:34 +0800

    Create payment for the mall.

--- app/Http/Controllers/User/MallController.php
+++ app/Http/Controllers/User/MallController.php
@@ -18,4 +18,21 @@ class MallController extends Controller
         return view('user.mall.index')->withTitle(trans('mall.title'))
             ->withCombos($products->where('type', '=', 'combo')->orderBy('price', 'DESC')->get());
     }
+
+    public function payment()
+    {
+        $discount = 0;
+        $input = request()->all();
+
+        $fee_rate = 0.01;
+        $unit_price = Products::find($input['product'])->price;
+        $fee = ($input['index'] + 1) * $unit_price * $fee_rate;
+        $orig = ($input['index'] + 1) * $unit_price + $fee;
+
+        if ($input['payment'] == 'alipay') {
+            $discount = $discount + $fee;
+        }
+
+        $subtotal = $orig - $discount;
+    }
 }
