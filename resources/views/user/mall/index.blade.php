88f4584d30b4ec0ae1766f9cd268727584ef3cd6
Frank Kung <kfanjian@gmail.com>
2016-05-21 00:10:57 +0800

    fix.

--- resources/views/user/mall/index.blade.php
+++ resources/views/user/mall/index.blade.php
@@ -31,8 +31,8 @@
                                     <div class="checkbox icheck">
                                         <div class="row">
                                         @foreach ($combos as $combo)
-                                        @if ($hasCombo)
-                                            @if ($combo->amount == $hasCombo->combo)
+                                        @if ($has_combo)
+                                            @if ($combo->amount == $has_combo->combo)
                                                 <div class="col-xs-6 col-md-3">
                                                     <input type="radio" name="product" data-price="{{$combo->price}}" class="flat-blue" value="{{$combo->id}}"/>
                                                     <span style="font-weight: 900;font-size: 1.2em;">{{$combo->name}}</span>
