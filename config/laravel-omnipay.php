cc19a399275dc71cb078b53631a04cba64bfb51e
Frank Kung <kfanjian@gmail.com>
2016-05-24 06:49:33 +0800

    Config alipay version 2 gateway.

--- config/laravel-omnipay.php
+++ config/laravel-omnipay.php
@@ -17,6 +17,16 @@ return [
                 'notifyUrl' => env('ALIPAY_NOTIFY_URL'),
             ],
         ],
+        'alipay_v2' => [
+            "driver" => 'Alipay_Secured',
+            'options' => [
+                'partner' => env('ALIPAY_PARTNER'),
+                'key' => env('ALIPAY_APPID'),
+                'sellerEmail' => env('ALIPAY_SELLER_EMAIL'),
+                'returnUrl' => env('ALIPAY_RETURN_URL_V2'),
+                'notifyUrl' => env('ALIPAY_NOTIFY_URL_V2'),
+            ],
+        ],
         'paypal' => [
             'driver' => 'PayPal_Express',
             'options' => [
