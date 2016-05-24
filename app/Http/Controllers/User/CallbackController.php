405b26ea5fcaa92003655e37537af5229e36bc84
Frank Kung <kfanjian@gmail.com>
2016-05-25 07:40:49 +0800

    Record trade info.

--- app/Http/Controllers/User/CallbackController.php
+++ app/Http/Controllers/User/CallbackController.php
@@ -21,18 +21,24 @@ class CallbackController extends Controller
 
         config()->set('laravel-omnipay.gateways.alipay.driver', 'Alipay_SendGoods');
         $gateway = Omnipay::gateway();
-        $trade_status = Input::get('trade_status');
         $order_id = Input::get('out_trade_no');
+        $trade_no = $request->input('trade_no');
+        $trade_status = Input::get('trade_status');
+        $buyer_email = $request->input('buyer_email');
         $order = Order::where('order_id', '=', $order_id)->first();
 
         if (!$order) {
-            Log::error('callback.alipay.v2 not found order.', ['order_id' => $order_id]);
+            Log::error('callback.alipay.v2 not found order.', ['order_id' => $order_id,
+                'trade_no' => $trade_no, 'buyer_email' => $buyer_email,
+            ]);
             return;
         }
 
         // 1. update to WAIT_BUYER_PAY.
         if ($trade_status == 'WAIT_BUYER_PAY' and $order->state == 'ORDER_CREATED') {
-            Order::where('order_id', '=', $order_id)->update(['state' => 'WAIT_BUYER_PAY']);
+            Order::where('order_id', '=', $order_id)->update(['state' => 'WAIT_BUYER_PAY',
+                'trade_no' => $trade_no, 'buyer_email' => $buyer_email,
+            ]);
             return 'success';
         }
     }
