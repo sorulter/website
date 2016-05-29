d95ba40a6972674f240c4e4e3b30454d0fb5c9ac
Frank Kung <kfanjian@gmail.com>
2016-05-29 22:28:28 +0800

    Fix style.

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -40,7 +40,7 @@
                         <td>￥{{ $order->unit_price }}</td>
                         <td>{{ $order->quantity }}</td>
                         <td>￥{{ $order->amount }}</td>
-                        <td class="label label-info">{{ trans("mall." . mb_strtolower($order->state)) }}</td>
+                        <td><span class="label label-info">{{ trans("mall." . mb_strtolower($order->state)) }}</span></td>
                         <td>{{mb_substr($order->created_at, 5)}}</td>
                         <td>{{$order->updated_at}}</td>
                     </tr>
