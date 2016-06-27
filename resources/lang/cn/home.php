7451104e94dd3b24ca2310e882f5ddd334e5a6a8
Frank Kung <kfanjian@gmail.com>
2016-06-28 06:21:13 +0800

    Add Chinese Activate tips.

--- resources/lang/cn/home.php
+++ resources/lang/cn/home.php
@@ -20,7 +20,9 @@ return [
     'activate_guide' => '<p> &emsp;&emsp;欢迎使用iProxier（IPX）。我们需要验证您的邮箱来激活账号。</p>
     <ol>
         <li> 点击下面的“发送激活邮件”按钮；</li>
-        <li> 稍后点击我们发送给您的激活邮件。</li>
+        <li> 稍后点击我们发送给您的激活邮件；</li>
+        <li> 如果您没有收到邮件，<b>请检查您的邮件垃圾箱</b>；</li>
+        <li> 如果邮件在垃圾箱，请在邮箱网页上点击<b>【这不是垃圾邮件】</b>。</li>
     </ol>
     <p>&emsp;&emsp;您在<span class="label label-info">:before</span> 之前激活，将会获赠 <span class="label label-info">:free_flows MB</span> 试用流量。</p>
     <a href=":activate_send_url" class="btn btn-danger col-xs-12"><b>发送激活邮件</b></a>',
