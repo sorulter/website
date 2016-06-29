b4ec52e0db571d079c8893c2fff59ad009c3c7fc
Frank Kung <kfanjian@gmail.com>
2016-06-30 06:38:03 +0800

    Translate activate email.

--- resources/lang/cn/email.php
+++ resources/lang/cn/email.php
@@ -2,4 +2,12 @@
 
 return [
     'notification' => 'iProxer 系统通知(:date)',
+    'activate_account_title' => '请激活您在 iProxier 注册的帐号【:email】',
+    'activate_account_content' => '&emsp;&emsp;请点击下方的按钮激活您在 iProxier 注册的帐号',
+    'click_me_to_activate' => '点击我激活iProxier帐号！',
+    'nav' => [
+        'home_page' => '用户首页',
+        'mall' => '购买流量',
+        'helps' => '帮助手册',
+    ],
 ];
