52bbf93af6a8b59e11dc393b6f62fb63d5c04894
Frank Kung <kfanjian@gmail.com>
2016-06-30 06:37:44 +0800

    Update activate email template.

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,79 +1,117 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<html>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-<meta name="viewport" content="width=device-width">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Activate your iProxier account to start surfing</title>
+<title></title>
 <style type="text/css">
-img {
-max-width: 100%;
-}
-body {
-font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; height: 100% !important; line-height: 1.6; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; width: 100% !important;
-}
-body {
-background-color: #f6f6f6;
-}
-@media only screen and (max-width: 620px) {
-  table[class=body] h1 {
-    font-weight: 600 !important;
-  }
-  table[class=body] h2 {
-    font-weight: 600 !important;
-  }
-  table[class=body] h3 {
-    font-weight: 600 !important;
-  }
-  table[class=body] h4 {
-    font-weight: 600 !important;
-  }
-  table[class=body] h1 {
-    font-size: 22px !important;
-  }
-  table[class=body] h2 {
-    font-size: 18px !important;
-  }
-  table[class=body] h3 {
-    font-size: 16px !important;
-  }
-  table[class=body] .wrapper {
-    padding: 10px !important;
-  }
-  table[class=body] .content {
-    padding: 0 !important;
-  }
-  table[class=body] .container {
-    padding: 0 !important; width: 100% !important;
-  }
-  table[class=body] .btn {
-    width: 100% !important;
-  }
-}
+  #outlook a { padding: 0; }
+  .ReadMsgBody { width: 100%; }
+  .ExternalClass { width: 100%; }
+  .ExternalClass * { line-height:100%; }
+  body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+  table, td { border-collapse:collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+  img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
+  p { display: block; margin: 13px 0; }
 </style>
-</head>
-<body><table class="body" style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-spacing: 0; width: 100%; background: #f6f6f6;" width="100%"><tbody><tr>
-<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top"></td>
-		<td class="container" style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: block; max-width: 580px; width: 580px; margin: 0 auto; padding: 10px;" valign="top">
-			<div class="content" style="box-sizing: border-box; display: block; max-width: 580px; margin: 0 auto; padding: 10px;">
-<table class="main" style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 3px; border-spacing: 0; width: 100%; background: #fff; border: 1px solid #e9e9e9;" width="100%"><tbody><tr>
-<td class="wrapper" style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 30px;" valign="top">
-			<table style="box-sizing: border-box; -premailer-width: 100%; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%"><tbody><tr>
-<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top">
-						<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">Thanks for choosing iProxier. Please activate your account by clicking the button below.</p>
-						<table class="btn btn-primary" style="box-sizing: border-box; -premailer-width: auto; border-collapse: separate !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt; margin-bottom: 15px; width: auto;" width="auto"><tbody><tr>
-<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 5px; text-align: center; background: #348eda;" align="center" valign="top">
-              		<a href="https://www.iProxier.com/activate/{{ $code }}" style="box-sizing: border-box; border-radius: 5px; color: #fff; text-decoration: none; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; text-transform: capitalize; background: #348eda; margin: 0; padding: 12px 25px; border: 1px solid #348eda;">Activate iProxier account</a>
-                </td>
-              </tr></tbody></table>
-              <p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">This activate link will disable after <strong>15 minutes</strong>.</p>
-<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">We may need to communicate important service level issues with you from time to time, so it's <strong>important we have an up-to-date email address</strong> for you on file.</p>
-						<p style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-size: 14px; font-weight: normal; margin: 0 0 15px; padding: 0;">Thanks and happy surfing, The iProxier.</p>
-					</td>
-				</tr></tbody></table>
-</td>
-	</tr></tbody></table>
-</div>
-		</td>
-		<td style="box-sizing: border-box; vertical-align: top; mso-table-lspace: 0pt; mso-table-rspace: 0pt;" valign="top"></td>
-	</tr></tbody></table></body>
+<!--[if !mso]><!-->
+<style type="text/css">
+
+      @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+
+  </style>
+<style type="text/css">
+  @media only screen and (max-width:480px) {
+    @-ms-viewport { width:320px; }
+    @viewport { width:320px; }
+  }
+</style>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+<!--<![endif]-->
+<!--[if mso]>
+<xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+<style type="text/css">
+  @media only screen and (min-width:480px) {
+    .mj-column-per-36, * [aria-labelledby="mj-column-per-36"] { width:36%!important; }
+.mj-column-per-63, * [aria-labelledby="mj-column-per-63"] { width:63%!important; }
+.mj-column-per-100, * [aria-labelledby="mj-column-per-100"] { width:100%!important; }
+  }
+</style>
+<style type="text/css">
+    @media only screen and (max-width:480px) {
+      .mj-hero-content {
+        width: 100% !important;
+      }
+    }
+  </style></head>
+<body>
+  <div><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#367fa9;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#367fa9;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:10px 25px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:216px;">
+      <![endif]--><div aria-labelledby="mj-column-per-36" class="mj-column-per-36" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center"><div style="cursor:auto;color:#f0f0f0;font-family:helvetica;font-size:20px;font-weight:700;line-height:22px;"> iProxier.com </div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td><td style="vertical-align:top;width:378px;">
+      <![endif]--><div aria-labelledby="mj-column-per-63" class="mj-column-per-63" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;" align="center"><div><div class="mj-inline-links" style="width:100%;text-align:center;"><!--[if gte mso 9]>
+          <table border="0" cellpadding="0" cellspacing="0" align="center">
+            <tr>
+        <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.home_page') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user/mall" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.mall') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user/helps" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.helps') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+            </tr>
+          </table>
+        <![endif]--></div></div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#fafafa;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fafafa;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:600px;">
+      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="left"><div style="cursor:auto;color:#333;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:18px;line-height:22px;">{!! trans('email.activate_account_content') !!}</div></td></tr><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center"><table cellpadding="0" cellspacing="0" style="border:none;border-radius:3px;" align="center" border="0"><tbody><tr><td style="background:#f45e43;border-radius:3px;color:white;cursor:auto;" align="center" valign="middle" bgcolor="#f45e43"><a href="https://www.iProxier.com/activate/{{ $code }}" style="display:inline-block;text-decoration:none;background:#f45e43;border:1px solid #f45e43;border-radius:3px;color:white;font-family:&#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-size:13px;font-weight:normal;padding:10px 25px;" target="_blank">
+            {{ trans('email.click_me_to_activate') }}
+          </a></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#fbfbfb;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fbfbfb;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;"></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#f0f0f0;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#f0f0f0;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:600px;">
+      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="left"><div style="cursor:auto;color:#666;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;">copyright © 2016 iProxier.com
+          </div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></div>
+</body>
 </html>
