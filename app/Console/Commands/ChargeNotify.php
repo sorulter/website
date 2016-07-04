c4e1c86271529bded55e35a5f6d7760e24e23268
Frank Kung <kfanjian@gmail.com>
2016-07-05 06:29:07 +0800

    send notify mail.

--- app/Console/Commands/ChargeNotify.php
+++ app/Console/Commands/ChargeNotify.php
@@ -6,6 +6,7 @@ use App\User;
 use Carbon\Carbon;
 use DB;
 use Illuminate\Console\Command;
+use Mail;
 
 class ChargeNotify extends Command
 {
@@ -45,5 +46,16 @@ class ChargeNotify extends Command
                 ->where('combo_end_date', '<', Carbon::today()->addMonth()->format('Y-m-01'))
                 ->where(DB::raw('combo + forever + extra'), '<', env('FREE_FLOWS'));
         })->orderBy('id', 'DESC')->get();
+
+        $title = trans('email.charge_notify_title');
+        $btn = trans('email.charge_notify_btn');
+
+        foreach ($users as $k => $user) {
+            $args = ['user' => $user, 'title' => $title, 'msg' => trans('email.charge_notify_msg', ['email' => $user->email]),
+                'btn_title' => $btn, 'btn_link' => '/user/mall'];
+            Mail::laterOn('default', 10, 'emails.msg', $args, function ($m) use ($user, $title) {
+                $m->to($user->email)->subject($title);
+            });
+        }
     }
 }
