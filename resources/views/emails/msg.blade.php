d197e08378d262ab5f842a8bdb61ebf6d1204b9d
Frank Kung <kfanjian@gmail.com>
2016-07-04 21:44:10 +0800

    Update msg template.

--- resources/views/emails/msg.blade.php
+++ resources/views/emails/msg.blade.php
@@ -1,9 +1,130 @@
-<html>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-	<title></title>
-</head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title></title>
+<style type="text/css">
+  #outlook a { padding: 0; }
+  .ReadMsgBody { width: 100%; }
+  .ExternalClass { width: 100%; }
+  .ExternalClass * { line-height:100%; }
+  body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+  table, td { border-collapse:collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+  img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
+  p { display: block; margin: 13px 0; }
+</style>
+<!--[if !mso]><!-->
+<style type="text/css">
+
+      @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+
+  </style>
+<style type="text/css">
+  @media only screen and (max-width:480px) {
+    @-ms-viewport { width:320px; }
+    @viewport { width:320px; }
+  }
+</style>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+<!--<![endif]-->
+<!--[if mso]>
+<xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+<style type="text/css">
+  @media only screen and (min-width:480px) {
+    .mj-column-per-36, * [aria-labelledby="mj-column-per-36"] { width:36%!important; }
+.mj-column-per-63, * [aria-labelledby="mj-column-per-63"] { width:63%!important; }
+.mj-column-per-99, * [aria-labelledby="mj-column-per-99"] { width:99%!important; }
+.mj-column-per-100, * [aria-labelledby="mj-column-per-100"] { width:100%!important; }
+  }
+</style>
+<style type="text/css">
+    @media only screen and (max-width:480px) {
+      .mj-hero-content {
+        width: 100% !important;
+      }
+    }
+  </style></head>
 <body>
-<h2>{{$title}}</h2>
-{{$msg}}
+  <div><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#367fa9;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#367fa9;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:10px 25px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:216px;">
+      <![endif]--><div aria-labelledby="mj-column-per-36" class="mj-column-per-36" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center"><div style="cursor:auto;color:#f0f0f0;font-family:helvetica;font-size:20px;font-weight:700;line-height:22px;"> iProxier.com </div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td><td style="vertical-align:top;width:378px;">
+      <![endif]--><div aria-labelledby="mj-column-per-63" class="mj-column-per-63" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;" align="center"><div><div class="mj-inline-links" style="width:100%;text-align:center;"><!--[if gte mso 9]>
+  			  <table border="0" cellpadding="0" cellspacing="0" align="center">
+            <tr>
+  		  <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.home_page') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user/mall" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.mall') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+          <td style="padding: 15px 10px">
+        <![endif]--><a href="https://www.iproxier.com/user/help" style="display:inline-block;text-decoration:none;text-transform:uppercase;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;padding:15px 10px;">{{ trans('email.nav.helps') }}</a><!--[if gte mso 9]>
+          </td>
+        <![endif]--><!--[if gte mso 9]>
+            </tr>
+          </table>
+        <![endif]--></div></div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#2f323b;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#2f323b;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;padding-bottom:0px;padding-top:0px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:594px;">
+      <![endif]--><div aria-labelledby="mj-column-per-99" class="mj-column-per-99" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;padding-top:10px;padding-bottom:10px;padding-right:25px;padding-left:25px;" align="left"><div style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;">
+            <p><span style="font-weight: bold;"><span style="font-size: 18px;"><span style="color: rgb(250, 135, 57);">{{ $title | '鹳狸猿通知' }}</span></span></span></p>
+          </div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#fafafa;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fafafa;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:600px;">
+      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="left"><div style="cursor:auto;color:#333;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:18px;line-height:22px;">{{ $msg }}</div></td></tr><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center"><table cellpadding="0" cellspacing="0" style="border:none;border-radius:3px;" align="center" border="0"><tbody>
+@if (isset($btn_title) && isset($btn_link))
+      <tr><td style="background:#f45e43;border-radius:3px;color:white;cursor:auto;" align="center" valign="middle" bgcolor="#f45e43"><a href="https://www.iproxier.com/{{ $btn_link }}" style="display:inline-block;text-decoration:none;background:#f45e43;border:1px solid #f45e43;border-radius:3px;color:white;font-family:&#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-size:13px;font-weight:normal;padding:10px 25px;" target="_blank">
+            {{ $btn_title }}
+          </a></td></tr>
+@endif
+          </tbody></table></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+      <!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]--><div style="margin:0 auto;max-width:600px;background:#f0f0f0;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#f0f0f0;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;"><!--[if mso | IE]>
+      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:600px;">
+      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;"><table cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="left"><div style="cursor:auto;color:#666;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;">copyright © 2016 iProxier.com
+          </div></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]--></div>
 </body>
 </html>
