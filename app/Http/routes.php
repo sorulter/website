e0eb7d1d9697f95e4c7bb887b081a3cf9ba05b82
Frank Kung <kfanjian@gmail.com>
2016-07-24 23:58:25 +0800

    Routine of special user's orders for admin.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -110,6 +110,7 @@ Route::group(['prefix' => env('ADMINNS'), 'middleware' => ['auth', 'admin'], 'na
     Route::get('users/gift/{id}', ['as' => '/users/gift', 'uses' => 'UsersController@getGift']);
     Route::post('users/gift/{id}', ['as' => '/users/gift', 'uses' => 'UsersController@postGift']);
     Route::get('users/logs/{id}', ['as' => '/users/logs', 'uses' => 'UsersController@getLogs']);
+    Route::get('users/orders/{id}', ['as' => '/users/orders', 'uses' => 'UsersController@getOrders']);
 
     Route::get('category', ['as' => '/category', 'uses' => 'CategoryController@index']);
 
