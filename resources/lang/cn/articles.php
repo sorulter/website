7e4917bbd030ebfc2b6fcb5ba02803ed42a4ca71
Frank Kung <kfanjian@gmail.com>
2016-07-28 07:10:36 +0800

    Add articles CN language file.

---
+++ resources/lang/cn/articles.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Helps List' => '帮助信息列表',
+    'Title' => '标题',
+    'UpdatedAt' => '更新时间',
+    'Read' => '查看',
+    'Details' => '详情',
+    'created_at' => '发布于',
+    'back_to_list' => '返回列表',
+];
