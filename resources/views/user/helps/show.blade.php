0dbd65da7181c288c226f991d8a64d2d4b909dd7
Frank Kung <kfanjian@gmail.com>
2016-07-28 07:11:27 +0800

    Translate helps details page.

--- resources/views/user/helps/show.blade.php
+++ resources/views/user/helps/show.blade.php
@@ -8,7 +8,7 @@
         <h2 class="box-title">
             <span class="username"><a href="#">{{$help->title}}</a></span>
         </h2>
-        <small class="description pull-right">Published at - {{$help->updated_at}}</small>
+        <small class="description pull-right">{{ trans('articles.created_at') }} - {{$help->updated_at}}</small>
     </div><!-- /.box-header -->
     <div class="box-body table-responsive typora-export ">
         <div class="article-content">
@@ -16,7 +16,7 @@
         </div>
     </div>
     <div class="box-footer clearfix">
-        <a href="{{ route('user/helps', '') }}">Back to List</a>
+        <a href="{{ route('user/helps', '') }}">{{ trans('articles.back_to_list') }}</a>
     </div>
     <!-- /.box-body -->
 </div>
