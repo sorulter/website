6b776a887cf0099f53a289bf51fe4d883a250d52
Frank Kung <kfanjian@gmail.com>
2016-07-30 23:38:39 +0800

    Modify item's name of setting menu.

--- resources/views/user/settings/nav.blade.php
+++ resources/views/user/settings/nav.blade.php
@@ -4,7 +4,7 @@
 	</div>
 	<div class="box-body no-padding">
 		<ul class="nav nav-pills nav-stacked">
-			<li @if ($act == 'index') class="active" @endif><a href="{{ route('user/settings') }}">{{ trans('settings.profiles') }}</a></li>
+			<li @if ($act == 'index') class="active" @endif><a href="{{ route('user/settings') }}">{{ trans('settings.system') }}</a></li>
 			<li @if ($act == 'PAC') class="active" @endif><a href="{{ route('user/settings/pac') }}">{{ trans('settings.pac') }}</a></li>
 		</ul>
 	</div>
