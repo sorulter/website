488d9c0851fc5242f842d335661925bb7d862378
Frank Kung <kfanjian@gmail.com>
2016-08-10 07:22:29 +0800

    track invitation hash id.

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Model\Track;
+use App\User;
 use Carbon\Carbon;
+use Hashids;
 
 class TrackerController extends Controller
 {
@@ -57,4 +59,42 @@ class TrackerController extends Controller
         session(['track_id' => $tracker->id]);
     }
 
+    public function invitation($hash)
+    {
+        $uid = Hashids::connection('invitation')->decode($hash)[0];
+
+        $tracker = Track::firstOrNew([
+            'day' => Carbon::now()->toDateString(),
+            'type' => 'user',
+            'source' => $uid,
+            'ip' => request()->ip(),
+        ]);
+
+        $tracker->day = Carbon::now()->toDateString();
+        $tracker->type = 'user';
+        $tracker->source = $uid;
+        $tracker->referrer = request()->server('HTTP_REFERER');
+        $tracker->ip = request()->ip();
+        $tracker->count += 1;
+        $tracker->save();
+
+        $user = User::find($uid);
+        $email = $this->cut($user->email);
+        $invitation_msg = trans('base.invitation_by_user', ['email' => $email]);
+        session(['invitation_id' => $uid]);
+        session(['invitation_msg' => $invitation_msg]);
+
+        return redirect()->route('register');
+    }
+
+    private function cut($email)
+    {
+        $arr = mb_split('@', $email);
+        $size = mb_strlen($arr[0]);
+        if ($size > 4) {
+            return $arr[0][0] . $arr[0][1] . '***' . $arr[0][$size - 1] . '@' . $arr[1];
+        } else {
+            return '***@' . $arr[1];
+        }
+    }
 }
