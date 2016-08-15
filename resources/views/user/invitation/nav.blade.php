e4a599dd55cb2b10349a4de12f9dc711b06496dc
Frank Kung <kfanjian@gmail.com>
2016-08-15 23:40:31 +0800

    invitation nav blade.

---
+++ resources/views/user/invitation/nav.blade.php
@@ -0,0 +1,10 @@
+<div class="box box-solid">
+	<div class="box-header with-border">
+		<h3 class="box-title">{{ trans('settings.menu') }}</h3>
+	</div>
+	<div class="box-body no-padding">
+		<ul class="nav nav-pills nav-stacked">
+			<li @if ($act == 'index') class="active" @endif><a href="{{ route('user/invitation') }}">{{ trans('invitation.link') }}</a></li>
+		</ul>
+	</div>
+</div>
