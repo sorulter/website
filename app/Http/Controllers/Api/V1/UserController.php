cdacdb6f040a7279c6eda14a6db0cca6b8df2627
Frank Kung <kfanjian@gmail.com>
2016-09-01 21:50:18 +0800

    add comment.

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,6 +4,11 @@ namespace app\Http\Controllers\Api\V1;
 
 use Illuminate\Routing\Controller;
 
+/**
+ * User resource representation.
+ *
+ * @Resource("User", uri="/user")
+ */
 class UserController extends Controller
 {
     public function login()
