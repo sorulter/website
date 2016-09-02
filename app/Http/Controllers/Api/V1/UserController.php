3379e85ec9b133c13e8a9673a2017291ae466716
Frank Kung <kfanjian@gmail.com>
2016-09-02 23:56:24 +0800

    user login.

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -2,7 +2,9 @@
 
 namespace app\Http\Controllers\Api\V1;
 
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Routing\Controller;
+use JWTAuth;
 
 /**
  * User resource representation.
@@ -11,8 +13,35 @@ use Illuminate\Routing\Controller;
  */
 class UserController extends Controller
 {
+    use Helpers;
+
+    /**
+     * User login
+     *
+     * User login by email and password.
+     *
+     * @Get("/login")
+     * @Versions({"v1"})
+     */
     public function login()
     {
-        //
+        // grab credentials from the request
+        $credentials = request()->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['message' => trans('api.v1.invalid_credentials'), 'status_code' => 401], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['message' => trans('api.v1.could_not_create_token'), 'status_code' => 500], 500);
+        }
+
+        // all good so return the token
+        $message = trans('api.v1.login_success');
+        $status_code = 200;
+        return response()->json(compact('message', 'status_code', 'token'));
+    }
     }
 }
